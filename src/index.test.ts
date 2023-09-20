39f21e3f1783a1422fc0364306c462afe84e038e
louiss0 <louisshelton0gmail.com>
2023-09-20 17:49:51 -0400

    test: If class filter sorter sorts and filters utility classes.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,52 @@
-
-describe("Test is working an debug able", () => {
-
-
-    test("Can work", () => {
+import { classFilterAndSorter } from "."
 
 
 
+describe("Test class filters work as intended", () => {
 
-        expect("Hello World").toBeTruthy()
+
+    describe("Testing classFilterAndSorter", () => {
+
+
+
+        it("filters and sorts utility classes", () => {
+
+            const classes = "outline-solid outline-1 outline-gray-600 outline-[#FFF333]"
+
+            const sortedClasses = classFilterAndSorter(classes)
+
+
+            expect(sortedClasses).not.toBe(classes)
+
+
+            expect(sortedClasses.length).toBeLessThan(classes.length)
+
+            expect(sortedClasses).not.toBe("outline-solid outline-1 outline-gray-600")
+
+
+            expect(sortedClasses).not.toBe("outline-solid outline-1 outline-[#FFF333]")
+
+
+
+        })
+
+
+
+        it(
+            "sorts utility classes based on if it's a digit 1, word 2, color 3 , function 4, variable 5, or args last",
+            () => {
+
+                const classes = "random-solid random-[2_4_6] random-1 random-[#FFF333] random-[url(/foo)] random-[--foo]"
+
+
+                const sortedClasses = classFilterAndSorter(classes)
+
+                expect(sortedClasses).toBe("random-1 random-solid random-[#FFF333] random-[url(/foo)] random-[--foo] random-[2_4_6]")
+
+
+
+            })
+
 
 
 
