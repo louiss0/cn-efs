5b33f20ed4b9df3ccc9fe4d363ab1fd9919b3c09
louiss0 <louisshelton0gmail.com>
2023-11-01 14:41:36 -0400

    refactor: Doing an extra sanitty test by adding two more canventional bootstrap classes.

--- src/index.test.ts
+++ src/index.test.ts
@@ -243,11 +243,11 @@ describe("Testing Class Name Evaluator Filter Sorters work as intended", () => {
         it("filters based on conventional bootstrap colors", () => {
 
             const res = bootstrapCN_EFS(
-                "bg-primary bg-primary-emphasis"
+                "bg-primary bg-primary-emphasis text-warning-subtle text-warning"
             )
 
 
-            expect(res).toBe("bg-primary-emphasis")
+            expect(res).toBe("bg-primary-emphasis text-warning")
 
 
 
