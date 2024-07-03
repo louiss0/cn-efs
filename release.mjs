5a78a1911eae2a9a9303e997161b63f97a629efd
louiss0 <louisshelton0@gmail.com>
2024-07-02 23:20:57 -0400

    fix: Remove @semantic-release/npm
    JSR is used to publish the npm package not semantic release

--- release.mjs
+++ release.mjs
@@ -6,12 +6,6 @@ export default {
 	plugins: [
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
-		[
-			"@semantic-release/npm",
-			{
-				publish: false,
-			},
-		],
 		"@semantic-release/github",
 	],
 };
