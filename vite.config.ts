967b8d50b71e7ec34e37fbdbb00e045c75e53d22
louiss0 <louisshelton0@gmail.com>
2024-07-03 18:14:33 -0400

    fix: Adjust vitest to work properly.
    I needed to make these changes beacuse the Vitest VSCode extension told me to do so

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+    cacheDir: 'node_modules/.vitest',
     test: {
         globals: true,
-        cache: {
-            dir: 'node_modules/.vitest',
-        },
         environment: 'node',
         include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     }
